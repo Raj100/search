6ab2580a3e1b3b794f8ec94ee3b3b78978046de1
Raj Rawariya <98628762+Raj100@users.noreply.github.com>
2024-08-29 19:14:49 +0530

    search completed

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
-
 ## Getting Started
 
-First, run the development server:
+First make .env.local in the root directory with variable name as 
+```
+ZOMATO_API_KEY=
+```
+
+then,run the development server:
 
 ```bash
 npm run dev
--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -2,20 +2,24 @@ import { NextResponse } from 'next/server';
 import axios from 'axios';
 
 export async function GET(req: Request) {
-  try{
-  const instance = axios.create({
-        baseURL: 'https://www.weatherunion.com',
-        timeout: 1000,
-        headers: {'x-zomato-api-key': 'a606243db0db3805ec8273cc6dc2c797'}
-      });
-      const query = new URL(req.url);
-      const locality_id = query.searchParams.get('q');
-      const response = await instance.get(`/gw/weather/external/v0/get_locality_weather_data?locality_id=${locality_id}`);
-      console.log(response.data);
-      return NextResponse.json(response.data);
-    }
-    catch (error){
-      console.error(error);
-    }
-    return NextResponse.json({error: 'An error occurred'});
+  try {
+    const instance = axios.create({
+      baseURL: 'https://www.weatherunion.com',
+      timeout: 1000,
+      headers: {
+        'x-zomato-api-key': process.env.ZOMATO_API_KEY, 
+      },
+    });
+
+    const query = new URL(req.url);
+    const locality_id = query.searchParams.get('q');
+
+    const response = await instance.get(`/gw/weather/external/v0/get_locality_weather_data?locality_id=${locality_id}`);
+    
+    console.log(response.data);
+    return NextResponse.json(response.data);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: 'An error occurred' });
+  }
 }
